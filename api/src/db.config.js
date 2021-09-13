89ae3eef674221dba4726e2d02e3d4f28273ce43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-13 08:54:23 -0400

    add comments about env variables

--- api/src/db.config.js
+++ api/src/db.config.js
@@ -1,10 +1,21 @@
 const mysql = require('mysql');
 
+// read the environment variables defined on the host machine
+// Node.js allows one to look up all variables defined in the environment
+// and then access them using process.env.<VARIABLE_NAME>
+// https://nodejs.org/api/process.html#process_process_env
+
+// in our system we assume that the database is running
+// at the host URL "DB_HOST", and we can connect to it using
+// "DB_USER", "DB_PASSWORD" environment values
+// The database name comes from the "DB_NAME" variable.
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const database = process.env.DB_NAME;
 const password = process.env.DB_PASSWORD;
 
+// if the config variables are not set
+// throw an error and stop the application
 if (!host || !user || !database || !password) {
   throw new Error('DB variables are not set');
 }
